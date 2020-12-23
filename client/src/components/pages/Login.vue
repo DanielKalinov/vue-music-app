2460df8a719282fe9a07af3c2b215f03e3302db5
Daniel Kalinov <fogm6sujroo5hn@protonmail.com>
2020-12-23 12:25:21 +0200

    Added login and signup forms

--- client/src/components/pages/Login.vue
+++ client/src/components/pages/Login.vue
@@ -1,9 +1,27 @@
 <template>
-  <h1>Login page</h1>
+  <div id="login">
+    <form>
+      <h2>Login</h2>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" />
+      </div>
+      <button type="submit" class="button primary-btn">Log In</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#login {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- client/src/components/pages/Signup.vue
+++ client/src/components/pages/Signup.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1>Signup page</h1>
+  <div id="signup">
+    <form>
+      <h2>Signup</h2>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" />
+      </div>
+      <div class="form-group">
+        <label for="confirmPassword">Confirm password</label>
+        <input type="password" />
+      </div>
+      <button type="submit" class="button primary-btn">Sign Up</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#signup {
+  display: flex;
+  justify-content: center;
+}
+</style>
