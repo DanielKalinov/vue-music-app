12df2e922a9a461b64b9643e672e170c083f0549
Daniel Kalinov <fogm6sujroo5hn@protonmail.com>
2020-12-24 14:55:14 +0200

    Added error handling for signup form

--- client/src/components/pages/Login.vue
+++ client/src/components/pages/Login.vue
@@ -1,22 +1,50 @@
 <template>
   <div id="login">
-    <form>
+    <form @submit.prevent="onSubmit">
       <h2>Login</h2>
       <div class="form-group">
         <label for="email">Email</label>
-        <input type="text" />
+        <input type="text" name="email" v-model.trim="email" />
       </div>
       <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" />
+        <input type="password" name="password" v-model.trim="password" />
       </div>
-      <button type="submit" class="button primary-btn">Log In</button>
+      <button type="submit" :disabled="!formIsValid" class="button primary-btn">
+        Log In
+      </button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      emailIsValid: 'pending',
+      passwordIsValid: 'pending'
+    };
+  },
+  computed: {
+    formIsValid() {
+      return true;
+    }
+  },
+  methods: {
+    onSubmit() {
+      axios
+        .post('http://localhost:3000/login', {
+          email: this.email,
+          password: this.password
+        })
+        .then((res) => console.log(res));
+    }
+  }
+};
 </script>
 
 <style lang="scss">
--- client/src/components/pages/Signup.vue
+++ client/src/components/pages/Signup.vue
@@ -2,6 +2,7 @@
   <div id="signup">
     <form @submit.prevent="onSubmit">
       <h2>Signup</h2>
+      <div v-if="serverErr" class="server-err-message">{{ serverErr }}</div>
       <div class="form-group">
         <label for="email">Email</label>
         <input
@@ -10,7 +11,7 @@
           v-model.trim="email"
           @blur="validateEmail"
         />
-        <p v-if="!emailIsValid" class="form-err-message">
+        <p v-if="emailIsValid === false" class="form-err-message">
           Please enter a valid email
         </p>
       </div>
@@ -22,8 +23,8 @@
           v-model.trim="password"
           @blur="validatePassword"
         />
-        <p v-if="!passwordIsValid" class="form-err-message">
-          Please enter a valid password
+        <p v-if="passwordIsValid === false" class="form-err-message">
+          Password must be at least 8 characters long
         </p>
       </div>
       <button type="submit" :disabled="!formIsValid" class="button primary-btn">
@@ -41,8 +42,9 @@ export default {
     return {
       email: '',
       password: '',
-      emailIsValid: 'pending',
-      passwordIsValid: 'pending'
+      emailIsValid: '',
+      passwordIsValid: '',
+      serverErr: ''
     };
   },
   computed: {
@@ -67,7 +69,7 @@ export default {
       }
     },
     validatePassword() {
-      if (this.password) {
+      if (this.password.length >= 8) {
         this.passwordIsValid = true;
       } else {
         this.passwordIsValid = false;
@@ -75,8 +77,16 @@ export default {
     },
     onSubmit() {
       axios
-        .post('http://localhost:3000/signup')
-        .then((res) => console.log(res));
+        .post('http://localhost:3000/signup', {
+          email: this.email,
+          password: this.password
+        })
+        .then((res) => console.log(res.data))
+        .catch((err) => {
+          this.serverErr = err.response.data;
+          this.email = '';
+          this.password = '';
+        });
     }
   }
 };
