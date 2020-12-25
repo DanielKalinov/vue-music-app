752ecde4a1d5ed471c50b92d35f6333306d1a22e
Daniel Kalinov <fogm6sujroo5hn@protonmail.com>
2020-12-25 16:30:38 +0200

    Converted signUp and logIn controllers to async/await functions

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,39 +3,37 @@ const User = require('../models/user');
 module.exports.signUp = async (req, res) => {
   const { email, password } = req.body;
 
-  await User.findOne({ email })
-    .then((user) => {
-      if (user) {
-        throw Error('Email is already in use');
-      } else {
-        User.create({ email, password }).then((user) => {
-          req.session.userID = user._id;
-          res.send(user);
-        });
-      }
-    })
-    .catch((err) => {
-      if (err.message === 'Email is already in use') {
-        res.status(409).json(err.message);
-      } else {
-        res.status(500).json('Something went wrong. Please try again later');
-      }
-    });
-};
+  try {
+    const user = await User.findOne({ email });
 
-module.exports.logIn = (req, res) => {
-  const { email, password } = req.body;
-
-  User.login(email, password)
-    .then((user) => {
+    if (user) {
+      throw Error('Email is already in use');
+    } else {
+      const user = await User.create({ email, password });
       req.session.userID = user._id;
       res.send(user);
-    })
-    .catch((err) => {
-      if (err.message === 'Incorrect email or password') {
-        res.status(401).json(err.message);
-      } else {
-        res.status(500).json('Something went wrong. Please try again later');
-      }
-    });
+    }
+  } catch (err) {
+    if (err.message === 'Email is already in use') {
+      res.status(409).json(err.message);
+    } else {
+      res.status(500).json('Something went wrong. Please try again later');
+    }
+  }
+};
+
+module.exports.logIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    req.session.userID = user._id;
+    res.send(user);
+  } catch (err) {
+    if (err.message === 'Incorrect email or password') {
+      res.status(401).json(err.message);
+    } else {
+      res.status(500).json('Something went wrong. Please try again later');
+    }
+  }
 };
